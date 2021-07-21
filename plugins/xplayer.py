95732bcabe523809d14a3a7c821fb6f302eb22ba
Caleb Noble <uniqisuniq@outlook.com>
2021-07-21 07:30:43 +0530

    Update xplayer.py

--- plugins/xplayer.py
+++ plugins/xplayer.py
@@ -91,8 +91,8 @@ class XPlayer(GroupCallFactory):
         self.playlist = []
         self.chat_id = chat_id
         self.chat_has_bot = False
-        super().__init__(
-            (client=userge, path_to_log_file="").get_file_group_call(play_on_repeat=self.replay_songs)
+        super(client=userge, path_to_log_file="").get_file_group_call(
+            play_on_repeat=self.replay_songs
         )
 
     def start_playout(self, key: str):
